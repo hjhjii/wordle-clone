9c2f919f4a40c1a9c0503887e1c49b9409f949ed
Monkey <gigghi05@gmail.com>
2022-01-19 04:55:16 +0100

    added .gitignore

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-let word = "fdsfdsfd"
-fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word)
-    .then(res => {
-        if (res.status != 200) throw new Error("Sussy Error");
-        return res.json;
-    })
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
+function generateGuess() {
+
+}
+
+function checkAnswer(word) {
+    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word)
+        .then(res => {
+            if (res.status != 200) throw new Error("Sussy Error");
+            return res.json;
+        })
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+}
